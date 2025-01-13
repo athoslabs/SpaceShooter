db9f55b5cea43ea044f38d1e822db10d14077a74
Robert Morrow <rcmorrow01@verizon.net>
2025-01-13 14:05:11 -0500

    Implenented Spawn Manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,11 +17,19 @@ public class Player : MonoBehaviour
     [SerializeField]
     private int _lives = 3;
 
+    private SpawnManager _spawnManager;
+
     // Start is called before the first frame update
     void Start()
     {
         // Take the current position = new position (0,0,0)
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+
+        if(_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is NULL");
+        }
     }
 
     // Update is called once per frame
@@ -70,6 +78,7 @@ public class Player : MonoBehaviour
 
         if(_lives < 1)
         {
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
         }
 
