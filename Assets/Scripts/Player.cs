78039aad347fbadfa5cf03f513570393ae9d4641
Robert Morrow <rcmorrow01@verizon.net>
2025-01-13 12:22:29 -0500

    Added Enemy and damage logic for player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,9 @@ public class Player : MonoBehaviour
     private float _fireRate = 0.15f;
     private float _canFire = -1.0f;
 
+    [SerializeField]
+    private int _lives = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -59,6 +62,17 @@ public class Player : MonoBehaviour
          _canFire = Time.time + _fireRate;
         Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
         
+    }
+
+    public void Damage()
+    {
+        _lives--;
+
+        if(_lives < 1)
+        {
+            Destroy(this.gameObject);
+        }
+
     }
 
 }
